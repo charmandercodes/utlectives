fa5221577b4148a378960621f251fd305a36ad25
Rehaan Rahman <RehaanDjango@macbookpro.home>
2025-06-16 14:40:28 +1000

    got delete reviews working

--- a_users/urls.py
+++ a_users/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.userView , name='user-page'),
-    path('termsandconditions/', views.termsView, name='terms-and-conditions')
+    path('termsandconditions/', views.termsView, name='terms-and-conditions'),
+    path('delete-review/<int:review_id>/', views.deleteUserView, name='delete-review'),
 ]
--- a_users/views.py
+++ a_users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from a_reviews.models import Review
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required
 def userView(request):
     # get users reviews and feed to template
 
@@ -18,3 +20,16 @@ def userView(request):
 
 def termsView(request):
     return render(request, 'pages/terms.html')
+
+
+def deleteUserView(request, review_id):
+    review = Review.objects.get(id=review_id)
+
+    if request.method == 'POST':
+        review.delete()
+        return redirect('user-page')
+    
+    return render(request, 'a_users/review_confirm_delete.html', {'review': review})
+
+
+
