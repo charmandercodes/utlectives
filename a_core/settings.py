62ac2576c0db31a2aa041cf050e6fc9f05dd474b
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2025-09-15 19:41:48 +1000

    Update settings.py

--- a_core/settings.py
+++ a_core/settings.py
@@ -242,7 +242,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = 587
+if os.getenv("EMAIL_PORT"):
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 ACCOUNT_EMAIL_SUBJECT_PREFIX = '' 
